dd7b1a2b5501a5ab833d5833450e754df4aacdb1
Ryan Leong <ryan.leong@moneysmart.com>
2021-11-01 23:31:57 +0800

    Intial Setup

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Back 4 Blood Builds</title>
+        <meta name="description" content="Back 4 Blood Builds" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="container">
+        <h1 className="font-Roboto font-light text-5xl">Back 4 Blood Builds</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
