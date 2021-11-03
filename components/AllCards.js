8a318d7d7678583b316e043f9639a673b04fe1a1
Ryan Leong <ryan.leong@moneysmart.com>
2021-11-03 21:37:27 +0800

    Add structure for Cards

--- components/AllCards.js
+++ components/AllCards.js
@@ -1,9 +1,21 @@
 const AllCards = () => {
-  return (
-    <div className="flex-1 ml-4">
-      <h2 className="text-4xl">All Cards</h2>
-    </div>
-  );
+  const cards = [
+    { id: 1, name: "Card 1" },
+    { id: 2, name: "Card 2" },
+    { id: 3, name: "Card 3" },
+    { id: 4, name: "Card 4" },
+    { id: 5, name: "Card 5" },
+  ];
+
+  function renderCards() {
+    return cards.map(({ id }) => {
+      return (
+        <div key={id} className="h-80 w-60 mr-4 mb-4 rounded-md bg-gray-400" />
+      );
+    });
+  }
+
+  return <div className="flex flex-1 flex-wrap ml-4">{renderCards()}</div>;
 };
 
 export default AllCards;
