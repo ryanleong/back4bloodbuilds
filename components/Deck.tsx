aca3489f590c41477e1a4135778541c96e4a2299
Ryan Leong <ryan.leong@moneysmart.com>
2021-11-03 22:30:36 +0800

    Implement DnD for Deck

--- components/Deck.tsx
+++ components/Deck.tsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
+import { Container, Draggable } from "react-smooth-dnd";
+
 import { ICard, IDeckProps } from "../utils/types";
 
 const Deck = ({ cards }: IDeckProps): JSX.Element => {
+  const [currentCards, setCurrentCards] = useState<Array<ICard>>([]);
+
+  useEffect(() => {
+    setCurrentCards(cards);
+  }, []);
+
+  const onDrop = ({ addedIndex, removedIndex }): void => {
+    // Reorder cards in state
+    setCurrentCards((oldCards) => {
+      const newCards = [...oldCards];
+      const movedCard = newCards.splice(removedIndex, 1);
+      newCards.splice(addedIndex, 0, movedCard[0]);
+      return newCards;
+    });
+  };
+
   const renderCards = () => {
-    return cards.map(({ id }: ICard) => {
+    return currentCards.map(({ id, name }: ICard) => {
       return (
-        <div key={id} className="bg-gray-400 h-10 w-full mb-2 rounded-sm" />
+        <Draggable key={id}>
+          <div className="bg-gray-400 h-10 w-full mb-2 rounded-sm">
+            id: {id}, name: {name}
+          </div>
+        </Draggable>
       );
     });
   };
 
-  return <div className="w-80">{renderCards()}</div>;
+  return (
+    <div className="w-80">
+      <Container onDrop={onDrop}>{renderCards()}</Container>
+    </div>
+  );
 };
 
 export default Deck;
