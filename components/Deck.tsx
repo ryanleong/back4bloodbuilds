1f05d11d0da5b3d7103d37147431a3fe08d2c8de
Ryan Leong <ryan.leong@moneysmart.com>
2021-11-22 18:07:17 +0800

    Add mobile reorder

--- components/Deck.tsx
+++ components/Deck.tsx
@@ -47,34 +47,75 @@ const Deck = (props: IDeckProps): JSX.Element => {
   };
 
   /**
+   * @description Update card order on mobile
+   * @param {number} index current card index
+   * @param {number} newIndex - new index of card
+   */
+  const updateMobileOrder = (index: number, newIndex: number) => {
+    if (newIndex < 0 || newIndex >= deckCards.length) {
+      return;
+    }
+
+    updateDeckCardsOrder({
+      removedIndex: index,
+      addedIndex: newIndex,
+    });
+  };
+
+  /**
+   * @description Render mobile reorder component
+   * @param index current card index
+   * @returns {JSX.Element} mobile reorder component
+   */
+  const renderMobileReorder = (index: number) => {
+    return (
+      <div className="flex flex-col mr-4">
+        <button
+          className="h-6 w-6 mb-1"
+          onClick={() => updateMobileOrder(index, index - 1)}
+        >
+          <span className="material-icons">expand_less</span>
+        </button>
+
+        <span className="text-center">{index + 1}</span>
+
+        <button
+          className="h-6 w-6 mt-1"
+          onClick={() => updateMobileOrder(index, index + 1)}
+        >
+          <span className="material-icons">expand_more</span>
+        </button>
+      </div>
+    );
+  };
+
+  /**
    * @description Render cards
    * @returns {JSX.Element} - list of cards
-   * TODO: Add order change for mobile
    */
   const renderCards = () => {
-    return deckCards.map((card: ICard) => {
+    return deckCards.map((card: ICard, index) => {
       const { id, name, description, category } = card;
       const categoryColor = getBorderColorClass(category);
 
       return (
         <Draggable key={id}>
           <div
-            className={`w-full mb-2 border-l-4 flex flex-col py-2 pl-4 pr-2 bg-gray-800 ${categoryColor}`}
+            className={`flex flex-row w-full mb-2 border-l-4 py-2 pl-3 pr-2 bg-gray-800 ${categoryColor}`}
             onClick={() => console.log("here")}
           >
-            <div className="flex flex-row w-full">
+            {renderMobileReorder(index)}
+
+            <div className="flex flex-col">
               <span className="text-lg">{name}</span>
-
-              <div className="ml-auto flex flex-row items-center">
-                <button className="ml-1" onClick={() => removeFromDeck(card)}>
-                  <span className="material-icons">close</span>
-                </button>
-              </div>
-            </div>
-
-            <div>
               <span className="text-sm">{description}</span>
             </div>
+            <div className="ml-auto">
+              <button className="ml-1" onClick={() => removeFromDeck(card)}>
+                <span className="material-icons">close</span>
+              </button>
+            </div>
+            {/* <div className="flex flex-row w-full"></div> */}
           </div>
         </Draggable>
       );
